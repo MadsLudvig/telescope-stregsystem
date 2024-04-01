106f21c0092467ef1f8760d2bc15f5c4dc394cf7
Mads Ludvig Timm Fagerlund <mads@fagerlund.io>
2024-04-01 04:18:38 +0200

    Change title

--- lua/stregsystem/init.lua
+++ lua/stregsystem/init.lua
@@ -6,7 +6,7 @@ local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 --
-local title = "telescope-stregsystem"
+local title = "Felescope"
 
 stregsystem.setup = function(opts)
 	stregsystem.config = opts or {}
