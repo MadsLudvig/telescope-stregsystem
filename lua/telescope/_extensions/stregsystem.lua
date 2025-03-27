52ff10aef4a3e101d7e6914074f00b343b64a908
Mads Fagerlund <mads@fagerlund.io>
2025-03-27 20:54:14 +0100

    remove old files

--- lua/telescope/_extensions/stregsystem.lua
+++
@@ -1,9 +0,0 @@
-local stregsystem = require("stregsystem")
-return require("telescope").register_extension({
-	setup = function(ext_config, _)
-		stregsystem.setup(ext_config)
-	end,
-	exports = {
-		stregsystem = stregsystem.stregsystem,
-	},
-})
